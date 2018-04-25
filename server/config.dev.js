0928c4851698ad9854645abebd50a30a2b1e5699
hoangnong <hoang.nong.it@gmail.com>
2018-04-25 09:28:05 +0700

    Add remote mongo

--- server/config.dev.js
+++ server/config.dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   serverPort: 3000,
-  mongodb: 'mongodb://localhost/elapay',
+  // mongodb: 'mongodb://52.15.131.100:27017/elaPay', // local
+  mongodb: 'mongodb://52.15.131.100:27017/elaPay' // remote
   superSecret: 'super secret'
 }
