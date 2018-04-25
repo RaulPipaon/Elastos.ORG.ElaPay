fa10288310e159602ab2a0b09bcf265f157a41db
hoangnong <hoang.nong.it@gmail.com>
2018-04-25 09:38:54 +0700

    Fix bug missing comma

--- server/config.dev.js
+++ server/config.dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   serverPort: 3000,
   // mongodb: 'mongodb://52.15.131.100:27017/elaPay', // local
-  mongodb: 'mongodb://52.15.131.100:27017/elaPay' // remote
+  mongodb: 'mongodb://52.15.131.100:27017/elaPay', // remote
   superSecret: 'super secret'
 }
