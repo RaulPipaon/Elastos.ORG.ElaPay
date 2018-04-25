83daf34531b8551c73e2081d43302c6a2ddca203
hoangnong <hoang.nong.it@gmail.com>
2018-04-25 15:21:05 +0700

    Update api

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -4,5 +4,5 @@
 
 export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
-export const SERVER_API_URL = 'http://localhost:3001/';
+export const SERVER_API_URL = 'http://103.200.5.47:3001/';
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
