389f8754f5e8830e67da7afe3450bb15c62c641c
SharedMocha <tammareddy53@gmail.com>
2018-04-24 21:48:49 -0700

    Updated package json and all.js
    Updated package json and all.js

--- server/modules/api/routes/all.js
+++ server/modules/api/routes/all.js
@@ -1,10 +1,23 @@
+import elaprices from 'modules/api/routes/elaprice'
+import querytx from 'modules/api/routes/querytx'
+import subscribewithdetails from 'modules/api/routes/subscriptiondetails'
+import subscribewithtx from 'modules/api/routes/subscriptionhash'
 import {
   create as createOrder,
   orderDetail as getOrder
 } from '../controllers/order'
 
 
+
+
+
 export default app => {
   app.post('/order', createOrder)
   app.get('/order/:id', getOrder)
+  app.get('/elaprices', elaprices.details);
+  app.get('/querytx', querytx.details);
+  app.post('/subscribewithdetails', subscribewithdetails.details);
+  app.post('/subscribewithtx', subscribewithtx.details);
 }
+
+
