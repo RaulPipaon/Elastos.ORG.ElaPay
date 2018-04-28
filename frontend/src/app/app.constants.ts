1a3654967589ed8ae0fa380390a64db55ca1b58b
hoangnong <hoang.nong.it@gmail.com>
2018-04-28 17:29:47 +0700

    Update url call server aws

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -4,6 +4,6 @@
 
 export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
-export const SERVER_API_URL = 'http://103.200.5.47:3002/';
+export const SERVER_API_URL = 'http://18.204.152.59/';
 // export const SERVER_API_URL = 'http://192.168.187.132:3002/';
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
