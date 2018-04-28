8e1ea3c7364ad3fba6a5572ed597bdb2e40361d0
hoangnong <hoang.nong.it@gmail.com>
2018-04-29 00:18:23 +0700

    Update using cluster

--- server/index.js
+++ server/index.js
@@ -12,42 +12,62 @@ import querytx from 'modules/api/controllers/querytx'
 import subscribewithdetails from 'modules/api/controllers/subscriptiondetails'
 import subscribewithtx from 'modules/api/controllers/subscriptionhash'
 import bodyParser from 'body-parser';
-
-var constants = require("config/constants");
-const system = express()
-
-system.use(bodyParser.json());
-system.use(bodyParser.urlencoded({
-    extended: true
-}));
-system.use('/api', api)
-system.get('/', (req, res) => {
-    res.json('Landing page')
-})
-
 //Added  to support Background Jobs
 import childProcess from 'child_process';
-var _finalizedData = null,
-    _httpRequestArray = ['Request Details'];
 
-var data = {
-    'start': true,
-    'interval': constants.BACKGROUNDJOBSTIMEINTERVAL,
-    'content': _httpRequestArray
-};
+if (cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
 
-//To Handle requests based on tx hash,details and fetch blocks
-system._retrieveTxDetailsChild = childProcess.fork('./services/bcjobs/txDbRetriever');
-system._sendTxDetailsChild = childProcess.fork('./services/bcjobs/txCallbackPost');
-system._retrieveDetailsPerBlock = childProcess.fork('./services/bcjobs/detailsDbRetriever');
-system._sendDetailsPerBlock = childProcess.fork('./services/bcjobs/detailsCallbackPost');
-system._retrieveTxBlockDetails = childProcess.fork('./services/bcjobs/fetchblocks');
-//Start request based on hash
-system._retrieveTxDetailsChild.send(data);
-system._sendTxDetailsChild.send(data);
-system._retrieveTxBlockDetails.send(data);
-system._retrieveDetailsPerBlock.send(data);
-system._sendDetailsPerBlock.send(data);
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-system.use(morgan('dev'))
-system.listen(SERVER_PORT, () => console.log(`Server listen to :${SERVER_PORT}`))
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    var constants = require("config/constants");
+    const system = express()
+
+    system.use(bodyParser.json());
+    system.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    system.use('/api', api)
+    system.get('/', (req, res) => {
+        res.json('Landing page')
+    })
+
+    var _finalizedData = null,
+        _httpRequestArray = ['Request Details'];
+
+    var data = {
+        'start': true,
+        'interval': constants.BACKGROUNDJOBSTIMEINTERVAL,
+        'content': _httpRequestArray
+    };
+
+    //To Handle requests based on tx hash,details and fetch blocks
+    system._retrieveTxDetailsChild = childProcess.fork('./services/bcjobs/txDbRetriever');
+    system._sendTxDetailsChild = childProcess.fork('./services/bcjobs/txCallbackPost');
+    system._retrieveDetailsPerBlock = childProcess.fork('./services/bcjobs/detailsDbRetriever');
+    system._sendDetailsPerBlock = childProcess.fork('./services/bcjobs/detailsCallbackPost');
+    system._retrieveTxBlockDetails = childProcess.fork('./services/bcjobs/fetchblocks');
+    //Start request based on hash
+    system._retrieveTxDetailsChild.send(data);
+    system._sendTxDetailsChild.send(data);
+    system._retrieveTxBlockDetails.send(data);
+    system._retrieveDetailsPerBlock.send(data);
+    system._sendDetailsPerBlock.send(data);
+
+    system.use(morgan('dev'))
+    system.listen(SERVER_PORT, () => console.log(`Server listen to :${SERVER_PORT}`))
+}
