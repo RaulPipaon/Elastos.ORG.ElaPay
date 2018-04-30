bbd50703d6d1dca7e05eedbe586dc0ec2fab0e6c
hoangnong <hoang.nong.it@gmail.com>
2018-04-30 23:51:29 +0700

    Update url call server

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -4,6 +4,7 @@
 
 export const VERSION = process.env.VERSION;
 export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
-export const SERVER_API_URL = 'http://103.200.5.47:3002/';
+// export const SERVER_API_URL = 'http://103.200.5.47:3002/';
 // export const SERVER_API_URL = 'http://192.168.187.132:3002/';
+export const SERVER_API_URL = 'http://207.148.119.222:3002/';
 export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
