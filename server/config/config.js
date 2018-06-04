3e9259dbaf00c2022e01345b05369043230fa583
hoangnong <hoang.nong.it@gmail.com>
2018-06-04 22:30:50 +0700

    Add service send email by SES of AWS

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,10 @@
 export const SERVER_PORT = 3002
 export const MONGODB = 'mongodb://localhost/elaPay'
 // export const MONGODB = 'mongodb://52.15.131.100:27017/elaPay'
+
+export const AWS = {
+    region: 'your-region',
+    accessKeyId: 'your-access-key-id',
+    secretAccessKey: 'your-secret-access-key',
+    sourceMailAws: 'your-email-verify'
+}
---
+++ server/services/aws.js
@@ -0,0 +1,54 @@
+import AWS from 'config/aws'
+import { AWS as AWS_CONFIG } from 'config/config';
+
+/*
+* @subject: string
+* @toAddresses: array email []
+* @ccAddresses: array email []
+* @htmlFormatBody: html format
+* @textFormatBody: string format
+*/
+
+// Example for other file call service sendMail
+// import {sendMail} from 'services/aws';
+
+// const subject = 'Subject';
+// const toAddresses = ['hoang@gmail.com'];
+// const ccAddresses = ['hoang@gmail.com'];
+// const htmlFormatBody = '<p>aaaaaaa</p>';
+// const textFormatBody = 'aaaaaaaaaaaaaa';
+
+// sendMail(subject, toAddresses, ccAddresses, htmlFormatBody, textFormatBody);
+
+export function sendMail(subject, toAddresses, ccAddresses, htmlFormatBody, textFormatBody) {
+    // Create sendEmail params template
+    var params = {
+        Destination: { // required
+            CcAddresses: ccAddresses,
+            ToAddresses: toAddresses
+        },
+        Message: { // required
+            Body: { // required
+                Html: {
+                    Charset: "UTF-8",
+                    Data: htmlFormatBody
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: textFormatBody
+                    }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: subject
+            }
+        },
+        // Host: 'email-smtp.us-west-2.amazonaws.com',
+        Source: AWS_CONFIG.sourceMailAws, // required
+        ReplyToAddresses: [],
+    }
+
+    const ses = new AWS.SES({apiVersion: '2010-12-01'});
+
+    return ses.sendEmail(params).promise();
+}
